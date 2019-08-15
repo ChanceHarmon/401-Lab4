9ce55fae69e8b9d06d7ba300515998763dc2387e
Chance Harmon <henrikillsit@gmail.com>
2019-08-15 16:42:01 -0700

    one step

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -44,4 +44,30 @@ describe('Categories Model', () => {
       });
   });
 
+  it('can update() a name', () => {
+    let obj = { name: 'bob' };
+    return categories.create({ name: 'new name' })
+      .then(record => {
+        return categories.update(record.id, { id: record.id, name: 'bob' })
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+
 });
+
+// it('can delete() an id', () => {
+//   let obj = { record.id };
+//   return categories.create({ id: record.id })
+//     .then(record => {
+//       return categories.delete({ id: record._id })
+//         .then(record => {
+
+//           expect(record).toBe(false);
+//         });
+//     });
+// });
+
